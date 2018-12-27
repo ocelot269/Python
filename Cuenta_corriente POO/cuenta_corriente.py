c9b6534302d8a689ece6f8ce74f70ffed59dfcaf
unknown <ocelot269@gmail.com>
2018-12-27 19:43:10 +0000

    Pasado los 5 primeros casos test

--- Cuenta_corriente POO/cuenta_corriente.py
+++ Cuenta_corriente POO/cuenta_corriente.py
@@ -1,32 +1,38 @@
 class CuentaCorriente(object):
 
     def __init__(self, nombre, apellidos, direccion, telefono):
-        self.__nombre = str(nombre)
-        self.__apellidos = str(apellidos)
+        self.nombre = str(nombre)
+        self.apellidos = str(apellidos)
         self.__direccion = str(direccion)
-        self.__telefono = str(telefono)
+        self.telefono = str(telefono)
         self.__saldo = float(1000)
         self.__nif = True
 
-    def retirarDinero(self, cantidad_retirada):
-        self.__saldo -= self.cantidad_retirada  
+    def setRetirarDinero(self, cantidad_retirada):
+        self.__saldo -= cantidad_retirada
         return self.__saldo
 
-    def ingresarDinero(self, cantidad_ingresada):
-        self.__saldo += self.cantidad_ingresada
+    def setIngresarDinero(self, cantidad_ingresada):
+        self.__saldo += cantidad_ingresada
+        assert type(self.__saldo == str)
         return self.__saldo
 
-    def consultarCuenta(self,):
-        print(self.__saldo)
+    def getConsultarCuenta(self):
+        return "Nombre: " + self.nombre + self.apellidos + " Tu saldo es: " + str(self.__saldo) + "€"
 
-    def saldoNegativo(self,__saldo):
-        if self.saldo >= 0:
+    def getSaldoNegativo(self):
+        if self.__saldo < 0:
             return True
         return False
 
 
 if __name__ == '__main__':
 
-
-    cuenta = CuentaCorriente("Jose Antonio ", "Zamora Andres", "Calle Falsa nª63", "654564561",)
-    cuenta.consultarCuenta()
+    cuenta = CuentaCorriente(
+        "Jose Antonio ", "Zamora Andres", "Calle Falsa nª63", "654564561",)
+    assert cuenta.getConsultarCuenta(
+    ) == "Nombre: Jose Antonio Zamora Andres Tu saldo es: 1000.0€"
+    assert cuenta.getSaldoNegativo() == False
+    assert cuenta.setIngresarDinero(1000) == 2000
+    assert cuenta.setRetirarDinero(2100) == -100
+    assert cuenta.getSaldoNegativo() == True
